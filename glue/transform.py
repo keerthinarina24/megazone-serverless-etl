ea46a46c2db0d78f4824b8a82f23d64e137e30fc
Keerthi Narina <keerthinarina24@gmail.com>
2025-12-23 16:30:00 -0600

    Add partitioning and enhance README for Megazone submission

--- glue/transform.py
+++ glue/transform.py
@@ -1,11 +1,14 @@
 from pyspark.sql.functions import year, month, dayofmonth
 
+# Read raw JSON data from S3
 df = spark.read.json("s3://megazone-raw-clickstream-bucket/")
 
+# Add partition columns derived from timestamp
 df = df.withColumn("year", year("timestamp")) \
        .withColumn("month", month("timestamp")) \
        .withColumn("day", dayofmonth("timestamp"))
 
+# Write transformed data in partitioned Parquet format
 df.write \
   .mode("overwrite") \
   .partitionBy("year", "month", "day") \
