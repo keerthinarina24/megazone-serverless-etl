c682d19c646817d4e2befb486f00533cf676f956
Keerthi Narina <keerthinarina24@gmail.com>
2025-12-23 17:03:54 -0600

    Add partitioned Parquet output for Athena optimization'

--- glue/transform.py
+++ glue/transform.py
@@ -1,10 +1,10 @@
-from pyspark.sql.functions import year, month, dayofmonth
+from pyspark.sql.functions import year, month, day
 
 df = spark.read.json("s3://megazone-raw-clickstream-bucket/")
 
 df = df.withColumn("year", year("timestamp")) \
        .withColumn("month", month("timestamp")) \
-       .withColumn("day", dayofmonth("timestamp"))
+       .withColumn("day", day("timestamp"))
 
 df = df.filter(
     df.user_id.isNotNull() &
